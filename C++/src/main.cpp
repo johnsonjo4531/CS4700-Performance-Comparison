17d917ab4d61d635371105554bfb7f5a0740fe76
Jaden Miller <jadendmiller@gmail.com>
2017-03-11 14:20:39 -0700

    Initial checkin with IDE project based settings and folders

---
+++ C++/src/main.cpp
@@ -0,0 +1,26 @@
+/*
+ *  No License.
+ * 
+ * 
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Jaden Miller
+ *
+ * Created on March 11, 2017, 2:14 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+   return 0;
+}
+
